77a7010db8c5d18a27a5b967483b1b6c97d9d1ee
Sergei Zakamskov <berserg2010@gmail.com>
2022-01-08 00:22:54 +0300

    Part 8

---
+++ README.md
@@ -0,0 +1,52 @@
+## Управление миграциями
+
+Заходим внутрь контейнера
+
+        docker ps
+        docker exec -it [CONTAINER_ID] bash
+
+Внутри контейнера
+
+        # Обновить миргации
+        alembic upgrade head
+        
+        # Откатить миграции
+        alembic downgrade base
+
+[1]
+
+[2]
+
+[3]
+
+[4]
+
+[5]
+
+[6]
+> [validators](https://pydantic-docs.helpmanual.io/usage/validators/)
+>
+>[constrained-types](https://pydantic-docs.helpmanual.io/usage/types/#constrained-types)
+
+[7]
+> [Rainbow_table](https://en.wikipedia.org/wiki/Rainbow_table)
+>
+> [Salt](https://en.wikipedia.org/wiki/Salt_(cryptography))
+>
+> [jwt](https://www.jeffastor.com/blog/jwt.io)
+>
+> [bcrypt-calculator](https://www.dailycred.com/article/bcrypt-calculator)
+>
+> [passlib](https://passlib.readthedocs.io/en/stable/faq.html)
+>
+> [jwt introduction](https://jwt.io/introduction/)
+>
+> [jwt debugger](https://jwt.io/#debugger-io)
+
+[8]
+> [oauth2-jwt](https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/)
+>
+> [get-the-username-and-password](https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#get-the-username-and-password)
+>
+> [run-it](https://fastapi.tiangolo.com/tutorial/security/first-steps/#run-it)
+> 
---
+++ backend/app/tests/conftest.py
@@ -0,0 +1,91 @@
+import os
+import warnings
+
+import alembic
+import pytest
+from alembic.config import Config
+from asgi_lifespan import LifespanManager
+from databases import Database
+from fastapi import FastAPI
+from httpx import AsyncClient
+
+from core.config import SECRET_KEY, JWT_TOKEN_PREFIX
+from db.repositories.cleanings import CleaningsRepository
+from db.repositories.users import UsersRepository
+from models.cleaning import CleaningCreate, CleaningInDB
+from models.user import UserCreate, UserInDB
+from services import auth_service
+
+
+# Apply migrations at beginning and end of testing session
+@pytest.fixture(scope="session")
+def apply_migrations():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    os.environ["TESTING"] = "1"
+    config = Config("alembic.ini")
+    alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
+
+
+# Create a new application for testing
+@pytest.fixture
+def app(apply_migrations: None) -> FastAPI:
+    from api.server import get_application
+    return get_application()
+
+
+# Grab a reference to our database when needed
+@pytest.fixture
+def db(app: FastAPI) -> Database:
+    return app.state._db
+
+
+# Make requests in our tests
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncClient:
+    async with LifespanManager(app):
+        async with AsyncClient(
+            app=app,
+            base_url="http://testserver",
+            headers={"Content-Type": "application/json"}
+        ) as client:
+            yield client
+
+
+@pytest.fixture
+async def test_cleaning(db: Database) -> CleaningInDB:
+    cleaning_repo = CleaningsRepository(db)
+    new_cleaning = CleaningCreate(
+        name="fake cleaning name",
+        description="fake cleaning description",
+        price=9.99,
+        cleaning_type="spot_clean",
+    )
+    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning)
+
+
+@pytest.fixture
+async def test_user(db: Database) -> UserInDB:
+    new_user = UserCreate(
+        email="lebron@james.io",
+        username="lebronjames",
+        password="heatcavslakers",
+    )
+    user_repo = UsersRepository(db)
+    existing_user = await user_repo.get_user_by_email(email=new_user.email)
+
+    if existing_user:
+        return existing_user
+
+    return await user_repo.register_new_user(new_user=new_user)
+
+
+@pytest.fixture
+def authorized_client(client: AsyncClient, test_user: UserInDB) -> AsyncClient:
+    access_token = auth_service.create_access_token_for_user(user=test_user, secret_key=str(SECRET_KEY))
+    client.headers = {
+        **client.headers,
+        "Authorization": f"{JWT_TOKEN_PREFIX} {access_token}",
+    }
+    return client
