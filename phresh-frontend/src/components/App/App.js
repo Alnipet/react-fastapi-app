758005122778f6dbc8d48c76ae5f3d515347f08b
Sergei Zakamskov <berserg2010@gmail.com>
2022-04-25 23:56:44 +0300

    Правка подключения eui

--- phresh-frontend/src/components/App/App.js
+++ phresh-frontend/src/components/App/App.js
@@ -1,24 +1,27 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage, Layout } from "../../components";
-import LoginPage from "../LoginPage/LoginPage";
-import NotFoundPage from "../NotFoundPage/NotFoundPage";
-import ProfilePage from "../ProfilePage/ProfilePage";
-import RegistrationPage from "../RegistrationPage/RegistrationPage";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LandingPage, Layout } from '../../components';
+import LoginPage from '../LoginPage/LoginPage';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import ProfilePage from '../ProfilePage/ProfilePage';
+import RegistrationPage from '../RegistrationPage/RegistrationPage';
+import { EuiProvider } from '@elastic/eui';
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <EuiProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={ <LandingPage/> }/>
+            <Route path="/login" element={ <LoginPage/> }/>
+            <Route path="/profile" element={ <ProfilePage/> }/>
+            <Route path="/registration" element={ <RegistrationPage/> }/>
+            <Route path="*" element={ <NotFoundPage/> }/>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </EuiProvider>
   );
 }
